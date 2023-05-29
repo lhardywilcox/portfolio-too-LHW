3e939894be266d2ecde2525542a3a8675c934572
LHardyWilcox <motacycaryda@mac.com>
2023-05-29 13:43:50 -0700

    file structure and component changes

---
+++ portfolio-too/src/components/Page.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Navigation from './Navigation';
+import Portfolio from './Portfolio';
+import About from './About';
+import Resume from './Resume';
+import Contact from './Contact';
+
+export default function Page() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'About Me') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume/>;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* We are passing the currentPage from state and the function to update it */}
+      <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+    </div>
+  );
+}
